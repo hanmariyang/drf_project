edf10411e0261f48d5cf958ee8c661c0ff701328
hanmariyang <hanmari.code@gmail.com>
2022-10-27 15:43:41 +0900

    [생성/업데이트] articles APP, url 기본 설계
    1) urls : article_view, article_detail_view, comment_view, comment_detail_view, like_view
    
    2) views : ArticleView, ArticleDetailView, CommentView, CommentDetailView, LikeView

---
+++ articles/views.py
@@ -0,0 +1,33 @@
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+
+class ArticleView(APIView):
+    def get(self, request):
+        pass
+    def post(self, request):
+        pass
+
+class ArticleDetailView(APIView):
+    def get(self, request, article_id):
+        pass
+    def put(self, request, article_id):
+        pass
+    def delete(self, request, article_id):
+        pass
+
+class CommentView(APIView):
+    def get(self, request):
+        pass
+    def post(self, request):
+        pass
+
+class CommentDetailView(APIView):
+    def put(self, request, comment_id):
+        pass
+    def delete(self, request, comment_id):
+        pass
+
+class LikeView(APIView):
+    def post(self, request):
+        pass
